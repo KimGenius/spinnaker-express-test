8b319c9009e14df39044cd7b2f5384a010fca662
KimGenius <youngjae1047@naver.com>
2017-10-31 10:16:13 +0900

    ???

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
+var server = app.listen(8080, function(){
+    console.log("Express server has started on port 8080")
 });
 
 app.use(express.static('public'));
